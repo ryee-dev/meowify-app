23f3f8bacf16180f1d04cf46374c22463dd44a44
roger yee <ryee.epicodus@gmail.com>
2018-05-15 23:44:31 -0700

    trying to show 'kitty creator page' to show

--- app/controllers/kittycreator_controller.rb
+++ app/controllers/kittycreator_controller.rb
@@ -1,22 +1,36 @@
 class KittycreatorController < ApplicationController
   def index
-    # @user = User.find(params[:id])
-    # @cat = @user.cats.new(cat_params)
-    # if @cat.save
-    #   redirect_to user_path(@cat.user)
-    # end
+    @cats = Cat.all
+    @cat = Cat.new
+
+    @cat.user_id = current_user.id
+    # render :index
+  end
+
+  def new
+    @cat = Cat.new
+    render :new
   end
 
   def create
     # @cat = Cat.new(cat_params)
-    # if @cat.save
-    #   redirect_to user_path(@cat.user)
+    # @cat.user_id = current_user.id
+    # if @cat.save!
+    #   redirect_to user_path(@cat.user_id)
+    # else
+    #   render :new
     # end
-    redirect_to kittycreator_index_path
+    @cat = Cat.new
   end
 
-  # private
-  # def cat_params
-  #   params.require(:cat).permit(:name, :caption, :breed, :expression, :pose)
-  # end
+  def show
+    @cats = Cat.all
+    @cat = Cat.find(params[:id])
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :caption, :breed, :expression, :pose)
+  end
+
 end
